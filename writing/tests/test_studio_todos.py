5b4ac5a65382be0f46bddd01f5adede76f58bd0f
Ali Zareh <ali.zareh.official@gmail.com>
2026-09-07 00:29:52 +0000

    chore: format; studio to-do screenshot

--- writing/tests/test_studio_todos.py
+++ writing/tests/test_studio_todos.py
@@ -11,7 +11,10 @@ TODO_RE = re.compile(r"%\s*(TODO|FIXME|XXX|HACK)\b[:\s-]*(.*)|\\todo(?:\[[^\]]*\
 def test_marker_grammar():
     assert TODO_RE.search("% TODO: fix the table").group(2) == "fix the table"
     assert TODO_RE.search("%FIXME power analysis").group(1) == "FIXME"
-    assert TODO_RE.search(r"text \todo[inline]{cite the replication} more").group(3) == "cite the replication"
+    assert (
+        TODO_RE.search(r"text \todo[inline]{cite the replication} more").group(3)
+        == "cite the replication"
+    )
     assert TODO_RE.search("% just a comment") is None
     assert TODO_RE.search(r"\section{TODO list}") is None  # a heading, not a marker
 
